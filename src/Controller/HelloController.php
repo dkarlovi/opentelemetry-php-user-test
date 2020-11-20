ea0b537d9a47c99e4272f24aea366453d0679d2a
Dalibor Karlović <dalibor.karlovic@flexolabs.io>
2020-11-20 12:33:44 +0100

    Child span in HTTP

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use OpenTelemetry\Sdk\Trace\Span;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,18 @@ class HelloController extends AbstractController
      */
     public function index(): Response
     {
+        global $tracer;
+        if ($tracer) {
+            /** @var Span $span */
+            $span = $tracer->getActiveSpan();
+            
+            $span->setAttribute('foo', 'bar');
+            $span->updateName('New name');
+
+            $tracer->startAndActivateSpan('Child span');
+            usleep(30000);
+            $tracer->endActiveSpan();
+        }
         usleep(30000);
         
         return new Response('Hello World');
