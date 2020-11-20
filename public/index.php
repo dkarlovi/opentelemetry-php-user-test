a62e8e313255cd9eacaefdbbdf615996fc33190c
Dalibor Karlović <dalibor.karlovic@flexolabs.io>
2020-11-20 12:07:54 +0100

    Add OpenTelemetry support for HTTP

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Kernel;
+use OpenTelemetry\Contrib\Jaeger\Exporter as JaegerExporter;
+use OpenTelemetry\Sdk\Trace\Clock;
+use OpenTelemetry\Sdk\Trace\Sampler\AlwaysOnSampler;
+use OpenTelemetry\Sdk\Trace\SamplingResult;
+use OpenTelemetry\Sdk\Trace\SpanProcessor\BatchSpanProcessor;
+use OpenTelemetry\Sdk\Trace\TracerProvider;
+use OpenTelemetry\Trace as API;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,8 +30,34 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
 
+$sampler = new AlwaysOnSampler();
+$samplingResult = $sampler->shouldSample(
+    null,
+    md5((string) microtime(true)),
+    substr(md5((string) microtime(true)), 16),
+    'io.opentelemetry.example',
+    API\SpanKind::KIND_INTERNAL
+);
+
+$exporter = new JaegerExporter(
+    'My App name Web Server',
+    'http://localhost:9412/api/v2/spans'
+);
+
+if (SamplingResult::RECORD_AND_SAMPLED === $samplingResult->getDecision()) {
+    $tracer = (new TracerProvider())
+        ->addSpanProcessor(new BatchSpanProcessor($exporter, Clock::get()))
+        ->getTracer('io.opentelemetry.contrib.php');
+
+    $request = Request::createFromGlobals();
+    $span = $tracer->startAndActivateSpan($request->getUri());
+}
+    
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
+if (SamplingResult::RECORD_AND_SAMPLED === $samplingResult->getDecision()) {
+    $tracer->endActiveSpan();
+}
